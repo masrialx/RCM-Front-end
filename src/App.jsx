c9755c37d105305cf425edf8747929b3b9901f58
Masresha Alemu <masrialemu@gmail.com>
2025-09-28 10:53:16 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RCM Frontend Application</h1>
+        <p>Welcome to the Revenue Cycle Management System</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
